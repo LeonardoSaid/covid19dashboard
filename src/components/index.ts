3a0156c626f52acd18eef27cc0c5f7bd49fbc8b4
Leonardo Said <leonardocosta013@gmail.com>
2020-08-27 16:27:27 -0300

    updated index and pie mock data

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,17 @@
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import BarChart from './BarChart/BarChart';
+import StateTable from './StateTable/StateTable';
+import PieChart from './PieChart/PieChart';
+import StateSearch from './StateSearch/StateSearch';
+import StatisticCard from './StatisticCard/StatisticCard';
 
 export {
     Header,
-    Footer
+    Footer,
+    BarChart,
+    StateTable,
+    PieChart,
+    StateSearch,
+    StatisticCard
 }
